ca2889513d34a0920e3d4b7cfb049ae8acd7b862
Ryan West <raw.ryanawest@gmail.com>
2022-12-01 20:12:23 -0500

    trying tf

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-import keras
-from keras.models import *
+import tensorflow.keras
+from tensorflow.keras.models import *
 
 
 """
@@ -18,8 +18,9 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-# @st.cache
+
 model = load_model("models_deploy/Detect_Covid-2022-11-28--02-30-48.h5")
+# @st.cache
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
